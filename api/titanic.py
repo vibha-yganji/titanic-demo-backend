0f65895c3eae63decefe5b6231b6256c94dc50f4
Hannah <hannahj.g.reyes@gmail.com>
2024-03-20 09:09:15 -0700

    test

--- api/titanic.py
+++ api/titanic.py
@@ -1,26 +1,32 @@
-from flask import Blueprint, request, jsonify
-from flask_restful import Resource, Api
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-from model.titanics import TitanicPredictor
+from flask import Blueprint, jsonify, request  # jsonify creates an endpoint response object
+from flask_restful import Api, Resource # used for REST API building
 
-titanic_api = Blueprint('titanic_api', __name__, url_prefix='/api/titanic')
+from model.titanic import *
+
+titanic_api = Blueprint('titanic_api', __name__,
+                   url_prefix='/api/titanic')
+
+# API generator https://flask-restful.readthedocs.io/en/latest/api.html#id1
 api = Api(titanic_api)
-
-# Initialize the TitanicPredictor instance
-titanic_predictor = TitanicPredictor()
 
 class TitanicAPI(Resource):
     def post(self):
-        try:
             # Get passenger data from the API request
             data = request.get_json()  # get the data as JSON
-            modified_data = pd.DataFrame(data)  # create DataFrame from JSON
-            response = titanic_predictor.predict_survival_probability(modified_data)
+            data['alone'] = str(data['alone']).lower()
+            converted_dict = {key: [value] for key, value in data.items()}
+            pass_in = pd.DataFrame(converted_dict)  # create DataFrame from JSON
+            titanic_predictor = TitanicPredictor()
+            titanic_predictor.load_data()
+            titanic_predictor.preprocess_data()
+            titanic_predictor.train_models()
+            titanic_predictor.evaluate_models()
+            dead_proba, alive_proba = titanic_predictor.predict_survival_probability(pass_in)
+            response = {
+                'dead_proba': dead_proba,  # Example probabilities, replace with actual values
+                'alive_proba': alive_proba
+            }
             return jsonify(response)
-
-        except Exception as e:
-            return jsonify({'error': str(e)})
 
 
 # Add resource to the API
